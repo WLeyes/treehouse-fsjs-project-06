c2b26efb06650f71bd56d4603d0af4d90150ce09
Warren Leyes <warren.leyes@gmail.com>
2018-09-21 18:41:08 -0700

    updated to use nodemon

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/about', (req, res, next) => {
+  res.render('about');
+});
+
+router.get('/project', (req, res, next) => {
+  res.render('project');
+});
+
 module.exports = router;
